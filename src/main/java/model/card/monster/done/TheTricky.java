dede65dada8728965d45ae2bcb2a1c153bd17734
Zahra <azar.zahra@yahoo.com>
2021-04-18 19:05:59 +0430

    gitignore

--- src/main/java/model/card/monster/done/TheTricky.java
+++ src/main/java/model/card/monster/done/TheTricky.java
@@ -6,8 +6,6 @@ import model.card.Card;
 import model.card.monster.Monster;
 
 public class TheTricky extends Monster {
-    public TheTricky() {
-    }
 
     public String action(int handIndex, Game game) {
         Board board = game.getCurrentPlayer().getBoard();
