8038046cd3e870cc359eb484fe1c9b509a00b8a5
dorsa <dorsaanaji@gmail.com>
2021-04-20 00:24:16 +0430

    updated deck class

--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -3,6 +3,8 @@ package model;
 import model.card.Card;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Deck {
     private String name;
@@ -13,18 +15,65 @@ public class Deck {
         this.name = name;
     }
 
-    public void removeCard(Card card) {
+    public void addCardToSideDeck(Card card) {
+        sideDeck.add(card);
     }
 
+    public void addCardToMainDeck(Card card) {
+        mainDeck.add(card);
+    }
+
+    public void removeCardFromMain(Card card) {
+        for (Card c : mainDeck) {
+            if (c.equals(card)) {
+                mainDeck.remove(card);
+                return;
+            }
+        }
+    }
+
+    public void removeCardFromSide(Card card) {
+        for (Card c : sideDeck) {
+            if (c.equals(card)) {
+                sideDeck.remove(card);
+                return;
+            }
+        }
+    }
+
+    public ArrayList<Card> getSideDeck() {
+        return sideDeck;
+    }
 
     public ArrayList<Card> getMainDeck() {
         return mainDeck;
     }
 
-    public static void sort(ArrayList<Deck> decks) {///////
+    public boolean SideIsFull() {
+        return sideDeck.size() >= 15;
+    }
+
+    public boolean MainIsFull() {
+        return mainDeck.size() >= 60;
+    }
+
+    public void shuffle(String deck) {
+        switch (deck) {
+            case "Main" -> Collections.shuffle(mainDeck);
+            case "Side" -> Collections.shuffle(sideDeck);
+        }
+    }
+
+    public boolean deckIsValid() {
+        return this.mainDeck.size() <= 60 && this.mainDeck.size() >= 40;  //sidedeck!?
+    }
+
+    public static void sort(ArrayList<Deck> decks) {
+        decks.sort(Comparator.comparing(o -> o.name));
     }
 
     public String toString() {
-        return null;
+        return name;
     }
+
 }
