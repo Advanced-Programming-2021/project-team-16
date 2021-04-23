d2a982fc9a226b73cd27f0e99505b87c3a52a251
DnyaNvB <dnya.nvb@gmail.com>
2021-04-24 03:54:36 +0430

    trap class

--- src/main/java/model/card/trap/CallOfTheHaunted.java
+++ src/main/java/model/card/trap/CallOfTheHaunted.java
@@ -1,7 +1,10 @@
 package model.card.trap;
 
 
+import model.Board;
 import model.Game;
+import model.card.Card;
+import model.card.monster.Monster;
 
 public class CallOfTheHaunted extends Trap {
     public CallOfTheHaunted() {
@@ -11,9 +14,18 @@ public class CallOfTheHaunted extends Trap {
                 "When that monster is destroyed, destroy this card.", "Unlimited", 3500);
     }
 
-    @Override
-    public String action() {
-        Game game = Game.getInstance();
-        return action();
+    public String action(Game game, String monsterName) {// dar tabe attack takmil shavad.  va inke az grave random niyaze ya card ruyi bardashte mishe?
+
+        Board board = game.getCurrentPlayer().getBoard();
+        Card monster = getCardByName(monsterName);
+        game.removeCardFromZone(monster, Board.Zone.GRAVE, game.getSelectedZoneIndex(), board);
+        if (monster instanceof Monster) {
+            game.putCardInZone(monster, Board.Zone.MONSTER, Board.CardPosition.ATK, board);
+        }
+        if (!(monster instanceof Monster)) {
+            game.putCardInZone(monster, Board.Zone.SPELL_AND_TRAP, Board.CardPosition.ATK, board);
+
+        }
+        return "done!";
     }
 }
