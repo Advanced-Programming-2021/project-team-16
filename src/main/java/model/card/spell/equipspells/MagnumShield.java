7294de4e9be8ca84c4d9b3dcf3066fb960a80029
dorsa <dorsaanaji@gmail.com>
2021-04-24 16:21:20 +0430

    finished equip spells

--- src/main/java/model/card/spell/equipspells/MagnumShield.java
+++ src/main/java/model/card/spell/equipspells/MagnumShield.java
@@ -1,9 +1,14 @@
 package model.card.spell.equipspells;
 
+import model.Board;
 import model.Game;
+import model.card.Card;
+import model.card.monster.Monster;
 import model.card.spell.Spell;
 
 public class MagnumShield extends Spell {
+    boolean doesExist;
+
     public MagnumShield() {
         super("Magnum Shield", "Spell", SpellType.EQUIP
                 , "Equip only to a Warrior-Type monster. Apply this effect, depending on its battle position." +
@@ -11,8 +16,41 @@ public class MagnumShield extends Spell {
                         "● Defense Position: It gains DEF equal to its original ATK.", "Unlimited", 4300);
     }
 
-    public void action(Game game) {
-
+    public String action(Game game, Card givenMonster, int indexOfThisMonster) {
+        Monster[] monsterZoneCurr = game.getCurrentPlayer().getBoard().getMonsterZone();
+        Monster[] monsterZoneRiv = game.getRival().getBoard().getMonsterZone();
+        Board.CardPosition[] currMonsterPositions = game.getCurrentPlayer().getBoard().getCardPositions()[0];
+        for (Monster monster : monsterZoneCurr) {
+            if (monster == givenMonster) {
+                doesExist = true;
+                break;
+            }
+        }
+        for (Monster monster : monsterZoneRiv) {
+            if (monster == givenMonster) {
+                doesExist = true;
+                break;
+            }
+        }
+        if (!doesExist) return "given monster is neither in rival's monster zone nor current player's.";
+        if (((Monster) givenMonster).getMonsterType() != Monster.MonsterType.WARRIOR)
+            return "Only Warrior monster can get equipped by this card";
+        if (monsterZoneCurr[indexOfThisMonster] == givenMonster) {
+            Board board = game.getCurrentPlayer().getBoard();
+            if (board.getCardPositions()[0][indexOfThisMonster] == Board.CardPosition.ATK)
+                ((Monster) givenMonster).ATK += ((Monster) givenMonster).getDEF();
+            if (board.getCardPositions()[0][indexOfThisMonster] != Board.CardPosition.ATK)
+                ((Monster) givenMonster).DEF += ((Monster) givenMonster).getATK();
+        }
+        if (monsterZoneRiv[indexOfThisMonster] == givenMonster) {
+            Board board = game.getRival().getBoard();
+            if (board.getCardPositions()[0][indexOfThisMonster] == Board.CardPosition.ATK)
+                ((Monster) givenMonster).ATK += ((Monster) givenMonster).getDEF();
+            if (board.getCardPositions()[0][indexOfThisMonster] != Board.CardPosition.ATK)
+                ((Monster) givenMonster).DEF += ((Monster) givenMonster).getATK();
+        }
+        return givenMonster + " is equipped successfully!";
 
     }
+
 }
--- src/main/java/model/card/spell/equipspells/UnitedWeStand.java
+++ src/main/java/model/card/spell/equipspells/UnitedWeStand.java
@@ -1,14 +1,44 @@
 package model.card.spell.equipspells;
 
+import model.Board;
+import model.Game;
+import model.card.Card;
+import model.card.monster.Monster;
 import model.card.spell.Spell;
 
 public class UnitedWeStand extends Spell {
+    Boolean doesExist;
+
     public UnitedWeStand() {
         super("United We Stand", "Spell", SpellType.EQUIP
                 , "The equipped monster gains 800 ATK/DEF for each face-up monster you control.", "Unlimited", 4300);
     }
 
-    public void action() {
+    public String action(Game game, Card givenMonster) {
+        int counter = 0;
+        Monster[] monsterZoneCurr = game.getCurrentPlayer().getBoard().getMonsterZone();
+        Monster[] monsterZoneRiv = game.getRival().getBoard().getMonsterZone();
+        for (Monster monster : monsterZoneCurr) {
+            if (monster == givenMonster) {
+                doesExist = true;
+                break;
+            }
+        }
+        for (Monster monster : monsterZoneRiv) {
+            if (monster == givenMonster) {
+                doesExist = true;
+                break;
+            }
+        }
+        if (!doesExist) return "given monster is neither in rival's monster zone nor current player's.";
+        Monster[] monsterZone = game.getCurrentPlayer().getBoard().getMonsterZone();
+        Board board = game.getCurrentPlayer().getBoard();
+        for (int i = 0; i < monsterZone.length; i++) {
+            if (monsterZone[i] != null && board.getCardPositions()[0][i] == Board.CardPosition.REVEAL_DEF ||
+                    board.getCardPositions()[0][i] == Board.CardPosition.ATK) counter++;
 
+        }
+        ((Monster) givenMonster).ATK += 800 * counter;
+        return givenMonster + " is equipped successfully!";
     }
 }
