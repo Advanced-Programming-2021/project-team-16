8339305d7bef0a3e02b710787e017e79d435fad3
dorsa <dorsaanaji@gmail.com>
2021-05-14 19:19:41 +0430

    ai

--- src/main/java/model/person/AI.java
+++ src/main/java/model/person/AI.java
@@ -4,10 +4,14 @@ import controller.GameMenu;
 import model.Board;
 import model.Deck;
 import model.Game;
+import model.card.Card;
 import model.card.monster.Monster;
+import model.card.spell.*;
+import model.card.trap.MagicCylinder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class AI extends Player {
     public AI() {
@@ -26,7 +30,104 @@ public class AI extends Player {
     }
 
     public void activeEffect() {
-        //درسا
+     Game game = GameMenu.getCurrentGame();
+     Card[] myHand = board.getHand();
+     Board rivalBoard = game.getRival().getBoard();
+     Card[] rivalSpellAndTrap = rivalBoard.getSpellAndTrapZone();
+        Monster[] rivalMonsters = rivalBoard.getMonsterZone();
+        Monster[] myMonsters = board.getMonsterZone();
+        for (int i = 0; i < myHand.length; i++) {
+           if(myHand[i] instanceof Spell){
+              if(myHand[i] instanceof HarpiesFeatherDuster){
+                  if(rivalSpellAndTrap != null && rivalSpellAndTrap.length > 2){
+                      game.setSelectedCard(myHand[i]);
+                      game.activeEffect();}
+              }
+
+              if(myHand[i] instanceof DarkHole){
+                  int sumOfRivalATK = 0;
+                  int sumOfMyATK = 0;
+                  if(rivalMonsters != null){
+                  for (int j = 0; j < rivalMonsters.length; j++) {
+                     sumOfRivalATK += rivalMonsters[i].getATK();
+                  }
+                  for (int j = 0; j < myMonsters.length; j++) {
+                      sumOfMyATK += myMonsters[i].getATK();
+                  }
+                  if(sumOfMyATK < sumOfRivalATK){
+                      game.setSelectedCard(myHand[i]);
+                      game.activeEffect();
+                  }
+                 }
+              }
+              if(myHand[i] instanceof ChangeOfHeart){
+                  int rivalBiggestATK = 0;
+                  int rivalsBiggestDEF = 0;
+                  int myBiggestATK = 0;
+                  int myBiggestDEF = 0;
+                  int rivalIndexFotATK = 0;
+                  int rivalIndexForDEF;
+                  for (int j = 0; j < Objects.requireNonNull(rivalMonsters).length; j++) {
+                    if  (rivalMonsters[i].getATK() > rivalBiggestATK) {
+                        rivalBiggestATK = rivalMonsters[i].getATK();
+                        rivalIndexFotATK = i;
+                    }
+                    if(rivalMonsters[i].getDEF() > rivalsBiggestDEF){
+                        rivalsBiggestDEF = rivalMonsters[i].getDEF();
+                        rivalIndexForDEF = i;
+                    }
+                  }
+                  for (int j = 0; j < myMonsters.length; j++) {
+                      if(myMonsters[i].getATK() > myBiggestATK)  myBiggestATK = myMonsters[i].getATK();
+                      if(myMonsters[i].getDEF() > myBiggestDEF) myBiggestDEF = myMonsters[i].getDEF();
+                  }
+                  if(rivalBiggestATK > rivalsBiggestDEF && rivalBiggestATK > myBiggestATK){
+                      Card monster = board.getCardByIndexAndZone(rivalIndexFotATK, Board.Zone.MONSTER);
+                      game.setSelectedCard(myHand[i]);
+                      game.activeEffect();
+                  }
+              }
+              if(myHand[i] instanceof PotOfGreed){
+                  if(myHand.length <= 2)
+                  game.setSelectedCard(myHand[i]);
+                  game.activeEffect();
+              }
+              if(myHand[i] instanceof  Raigeki){
+                  if(rivalMonsters != null && rivalMonsters.length > myMonsters.length){
+                      game.setSelectedCard(myHand[i]);
+                      game.activeEffect();
+                  }
+              }
+              if(myHand[i] instanceof RingOfDefense){
+                  boolean use = false;
+                  for (Card card: rivalSpellAndTrap) {
+                      if(card instanceof MagicCylinder)
+                          use = true;
+                  }
+                  if(use){
+                      game.setSelectedCard(myHand[i]);
+                      game.activeEffect();
+                  }
+              }
+              if(myHand[i] instanceof SupplySquad){
+                  game.setSelectedCard(myHand[i]);
+                  game.activeEffect();
+              }
+           }
+           if(myHand[i] instanceof SwordsOfRevealingLight) {
+               int faceDownMonsters = 0;
+               if (rivalMonsters != null) {
+
+                   for (int j = 0; j < rivalMonsters.length; j++) {
+                       if (board.getCardPositions()[0][i] == Board.CardPosition.HIDE_DEF) faceDownMonsters++;
+                   }
+                   if (faceDownMonsters > 2){
+                       game.setSelectedCard(myHand[i]);
+                       game.activeEffect();
+                   }
+               }
+           }
+        }
     }
 
     public void playBattlePhase() {
