6efe7bc344f37fca6e4c86455ebc02c7e58be55d
DnyaNvB <dnya.nvb@gmail.com>
2021-05-17 14:16:57 +0430

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,3 +9,144 @@ public class Main {
         UpdateStatus.afterRun();
     }
 }
+
+
+//   shop buy Baby dragon                                       :1600
+//   shop buy Baby dragon                                       :1600
+//   shop buy Baby dragon                                       :1600
+//   shop buy Curtain of the dark ones                                  :700
+//   shop buy Curtain of the dark ones                                  :700
+//   shop buy Curtain of the dark ones                                  :700
+//   shop buy Bitron                                                             :1000
+//   shop buy Bitron                                                             :1000
+//   shop buy Bitron                                                             :1000
+//   shop buy Battle warrior                                                             :1300
+//   shop buy Battle warrior                                                             :1300
+//   shop buy Battle warrior                                                             :1300
+//   shop buy Haniwa                                                               :600
+//   shop buy Haniwa                                                               :600
+//   shop buy Haniwa                                                               :600
+//   shop buy Exploder Dragon                                              :1000
+//   shop buy Exploder Dragon                                              :1000
+//   shop buy Exploder Dragon                                              :1000
+//   shop buy Flame manipulator                                   :1500
+//   shop buy Flame manipulator                                   :1500
+//   shop buy Flame manipulator                                   :1500
+//   shop buy Magnum Shield                                :800
+//   shop buy Magnum Shield                                :800
+//   shop buy Magnum Shield                                :800
+//   shop buy Marshmallon                                         :700
+//   shop buy Marshmallon                                         :700
+//   shop buy Marshmallon                                         :700
+//   shop buy Texchanger                                                    :200
+//   shop buy Texchanger                                                    :200
+//   shop buy Texchanger                                                    :200
+//   shop buy Wattkid                                                               :1300
+//   shop buy Wattkid                                                               :1300
+//   shop buy Wattkid                                                               :1300
+//   shop buy Yomi Ship                                                                      :1700
+//   shop buy Yomi Ship                                                                      :1700
+//   shop buy Yomi Ship                                                                      :1700
+//   shop buy Hero of the east                                                      :1700
+//   shop buy Hero of the east                                                      :1700
+//   shop buy Hero of the east                                                      :1700
+//   shop buy Scanner                                                     :1700
+//   shop buy MindCrush                                                   :2000
+//   shop buy Terraforming                                                :2500
+
+
+//   shop buy Command Knight                                                               :2100
+//   shop buy Dark Hole                                                                    :2500
+//   shop buy Black Pendant                                                                :4300
+//   shop buy Blue-Eyes white dragon
+//   shop buy ClosedForest                                                                 :4300
+//   shop buy Battle OX                                                                    :2900
+//   shop buy Crab Turtle                                                                 :10200
+//   shop buy Crawling dragon                                                              :3900
+//   shop buy Beast King Barbaros                                                          :9200
+//   shop buy Dark Blade                                                                   :3500
+//   shop buy Dark magician                                                                :8300
+//   shop buy Feral Imp                                                                    :2800
+//   shop buy Fireyarou                                                                    :2500
+//   shop buy Forest                                                                       :4300
+//   shop buy Gate Guardian                                                               :20000
+//   shop buy Harpie's Feather Duster                                                      :2500
+//   shop buy Herald of Creation                                                           :2700
+//   shop buy Horn Imp                                                                     :2500
+//   shop buy Leotron                                                                      :2500
+//   shop buy MagicCylinder                                                                :2000
+//   shop buy MagicJammer                                                                  :3000
+//   shop buy Advanced Ritual Art                                                          :3000
+//   shop buy Alexandrite Dragon                                                           :2600
+//   shop buy Axe Raider                                                                   :3100
+//   shop buy Messenger of peace                                                           :4000
+//   shop buy Monster Reborn                                                               :2500
+//   shop buy Mystical space typhoon                                                       :3500
+//   shop buy NegateAttack                                            :
+//   shop buy Ring of defense                                                              :3500
+//   shop buy Skull Guardian                                                               :7900
+//   shop buy Slot Machine                                                                 :7500
+//   shop buy SolemnWarning                                                                :3000
+//   shop buy Spell Absorption                                                             :4000
+//   shop buy Spiral Serpent                                                              :11700
+//   shop buy Suijin                                                                       :8700
+//   shop buy Supply Squad                                                                 :4000
+//   shop buy Sword of dark destruction                                                     4300
+//   shop buy Swords of Revealing Light                                                    :2500
+//   shop buy Terratiger, the Empowered Warrior                                            :3200
+//   shop buy The Calculator                                                               :8000
+//   shop buy The Tricky                                                                   :4300
+//   shop buy TimeSeal                                                                     :2000
+//   shop buy TorrentialTribute                                                            :2000
+//   shop buy TrapHole                                                                     :2000
+//   shop buy Twin Twisters                                                                :3500
+//   shop buy Umiiruka                                                                     :4300
+//   shop buy United We Stand                                                              :4300
+//   shop buy Warrior Dai Grepher                                                          :3400
+//   shop buy Wattaildragon                                                                :5800
+//   shop buy Yami                                                                         :4300
+
+
+//deck add-card --card Baby dragon --deck DonyasDeck
+//deck add-card --card Baby dragon --deck DonyasDeck
+//deck add-card --card Baby dragon --deck DonyasDeck
+//deck add-card --card Curtain of the dark ones --deck DonyasDeck
+//deck add-card --card Curtain of the dark ones --deck DonyasDeck
+//deck add-card --card Curtain of the dark ones --deck DonyasDeck
+//deck add-card --card Bitron --deck DonyasDeck
+//deck add-card --card Bitron --deck DonyasDeck
+//deck add-card --card Bitron --deck DonyasDeck
+//deck add-card --card Battle warrior --deck DonyasDeck
+//deck add-card --card Battle warrior --deck DonyasDeck
+//deck add-card --card Battle warrior --deck DonyasDeck
+//deck add-card --card Haniwa --deck DonyasDeck
+//deck add-card --card Haniwa --deck DonyasDeck
+//deck add-card --card Haniwa --deck DonyasDeck
+//deck add-card --card Exploder Dragon --deck DonyasDeck
+//deck add-card --card Exploder Dragon --deck DonyasDeck
+//deck add-card --card Exploder Dragon --deck DonyasDeck
+//deck add-card --card Flame manipulator --deck DonyasDeck
+//deck add-card --card Flame manipulator --deck DonyasDeck
+//deck add-card --card Flame manipulator --deck DonyasDeck
+//deck add-card --card Magnum Shield --deck DonyasDeck
+//deck add-card --card Magnum Shield --deck DonyasDeck
+//deck add-card --card Magnum Shield --deck DonyasDeck
+//deck add-card --card Marshmallon --deck DonyasDeck
+//deck add-card --card Marshmallon --deck DonyasDeck
+//deck add-card --card Marshmallon --deck DonyasDeck
+//deck add-card --card Texchanger --deck DonyasDeck
+//deck add-card --card Texchanger --deck DonyasDeck
+//deck add-card --card Texchanger --deck DonyasDeck
+//deck add-card --card Wattkid --deck DonyasDeck
+//deck add-card --card Wattkid --deck DonyasDeck
+//deck add-card --card Wattkid --deck DonyasDeck
+//deck add-card --card Yomi Ship --deck DonyasDeck
+//deck add-card --card Yomi Ship --deck DonyasDeck
+//deck add-card --card Yomi Ship --deck DonyasDeck
+//deck add-card --card Hero of the east --deck DonyasDeck
+//deck add-card --card Hero of the east --deck DonyasDeck
+//deck add-card --card Hero of the east --deck DonyasDeck
+//deck add-card --card Scanner --deck DonyasDeck
+//deck add-card --card MindCrush --deck DonyasDeck
+//deck add-card --card Terraforming --deck DonyasDeck
+
