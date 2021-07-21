48ef6f0b8725ea36c56e2ae12421ac7f1aefb9fc
DnyaNvB <dnya.nvb@gmail.com>
2021-07-21 23:54:56 +0430

    bubSE

--- src/main/java/server/MainServer.java
+++ src/main/java/server/MainServer.java
@@ -1,49 +1,152 @@
 package server;
 
+import controller.Login;
 import server.controller.ServerLoginController;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.util.*;
+import java.net.*;
 
 public class MainServer {
+    // Vector to store active clients
+    static Vector<ChatHandler> ar = new Vector<>();
+
+    // counter for clients
+    static int i = 0;
+    Scanner scn = new Scanner(System.in);
+    private String name;
+    static DataInputStream dataInputStream;
+    static DataOutputStream dataOutputStream;
+    static Socket socket;
+
+
+
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(1113);
+            ServerSocket serverSocket = new ServerSocket(8888);
             while (true) {
-                Socket socket = serverSocket.accept();
-                new Thread(() -> {
+                socket = serverSocket.accept();
+                System.out.println("New client request received : " + socket);
+//                new Thread(() -> {
                     try {
-                        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-                        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                        dataInputStream = new DataInputStream(socket.getInputStream());
+                        dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                        System.out.println("Creating a new handler for this client...");
                         while (true) {
-                            String input = dataInputStream.readUTF();
-                            String result = process(input);
+                            String received = dataInputStream.readUTF();
+                            System.out.println("Creating a new handler for this client...");
+                            if (received.startsWith("show-chatroom")) {
+                                System.out.println("welcomee");
+                                ChatHandler mtch = new ChatHandler(socket,"client " + i, dataInputStream, dataOutputStream);
+                                mtch.run();
+                                // Create a new Thread with this object.
+//                                Thread t = new Thread(mtch);
+
+                                System.out.println("Adding this client to active client list");
+
+                                // add this client to active clients list
+                                ar.add(mtch);
+
+                                // start the thread.
+//                                t.start();
+
+
+                                // increment i for new client.
+                                // i is used for naming only, and can be replaced
+                                // by any naming scheme
+                                i++;
+
+                            }
+
+                            String result = process(received);
                             if (result.equals("")) break;
                             dataOutputStream.writeUTF(result);
                             dataOutputStream.flush();
                         }
+
                         dataInputStream.close();
                         socket.close();
                         serverSocket.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }).start();
+//                }).start();
 
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
+    }
+// Java implementation of  Server side
+// It contains two classes : Server and ClientHandler
+// Save file as Server.java
+
+
+    // Server class
+
+
+    //
+//    public static void main(String[] args) throws IOException {
+//        // server is listening on port 1234
+//        ServerSocket ss = new ServerSocket(1234);
+//
+//        Socket s;
+//
+//        // running infinite loop for getting
+//        // client request
+//        while (true) {
+//            // Accept the incoming request
+//            s = ss.accept();
+//
+//            System.out.println("New client request received : " + s);
+//
+//            // obtain input and output streams
+//            DataInputStream dis = new DataInputStream(s.getInputStream());
+//            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+//            new Thread(() -> {
+//            String received;
+//            while (true) {
+//                try {
+//                    // receive the string
+//                    received = dis.readUTF();
+//
+//                    System.out.println(received);
+//                } catch (IOException e) {
+//
+//                    e.printStackTrace();
+//                } }
+//                }).start();
+//                    if (received.equals("show-chatroom")) {
+//                        // Create a new handler object for handling this request.
+//                        ChatHandler mtch = new ChatHandler(s, "client " + i, dis, dos);
+//
+//                        // Create a new Thread with this object.
+//                        Thread t = new Thread(mtch);
+//
+//                        System.out.println("Adding this client to active client list");
+//
+//                        // add this client to active clients list
+//                        ar.add(mtch);
+//
+//                        // start the thread.
+//                        t.start();
+//                    }
+//
+//
+//                // increment i for new client.
+//                // i is used for naming only, and can be replaced
+//                // by any naming scheme
+//                i++;
+//
+//            }
+//        }
+//    }
     static String process(String command) {
         if (command.startsWith("register")) {
             String[] parts = command.split(" ");
-           return ServerLoginController.signUp(parts[1], parts[2], parts[3]);
+            return ServerLoginController.signUp(parts[1], parts[2], parts[3]);
         } else if (command.startsWith("login")) {
             String[] parts = command.split(" ");
             return ServerLoginController.login(parts[1], parts[2]);
