21b78a8ad2303b9bb9e028078ab98be25df52d93
DnyaNvB <dnya.nvb@gmail.com>
2021-07-24 02:54:43 +0430

    bubSE22

--- src/main/java/server/ChatHandler.java
+++ src/main/java/server/ChatHandler.java
@@ -24,64 +24,46 @@ public class ChatHandler implements Runnable {
         this.dos = dos;
         this.name = name;
         this.s = s;
-        isloggedin=true;
+        isloggedin = true;
     }
+
     @Override
     public void run() {
 
         String received;
-        while (true)
-        {
-            try
-            {
-                // receive the string
+        try {
+            while (true) {
                 received = dis.readUTF();
 
                 System.out.println(received);
 
-                if(received.equals("logout")){
-                    this.isloggedin=false;
+                if (received.equals("logout")) {
+                    this.isloggedin = false;
                     this.s.close();
                     break;
                 }
+                String client = received.split("#", 2)[1];
+                String message = received.split("#", 2)[0];
 
-                // break the string into message and recipient part
-                StringTokenizer st = new StringTokenizer(received, "#");
-                String MsgToSend = st.nextToken();
-                String recipient = st.nextToken();
-
-                // search for the recipient in the connected devices list.
-                // ar is the vector storing client of active users
-                for (ChatHandler mc : MainServer.ar)
-                {
-                    // if the recipient is found, write on its
-                    // output stream
-                    if (mc.name.equals(recipient) && mc.isloggedin==true)
-                    {
-                        mc.dos.writeUTF(this.name+" : "+MsgToSend);
+                for (ChatHandler mc : MainServer.ar) {
+                    if (mc.name.equals(client) && mc.isloggedin) {
+                        mc.dos.writeUTF(this.name + " : " + message);
                         break;
                     }
                 }
-            } catch (IOException e) {
-
-                e.printStackTrace();
             }
+            System.out.println("badbalht shodim");
 
+        } catch (Exception e) {
+
+            e.printStackTrace();
         }
-        try
-        {
-            // closing resources
+        try {
             this.dis.close();
             this.dos.close();
 
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-//    @Override
-//    public void run() {
-//
-
-//    }
 }
