e5deaae08762e2122d372166e715aaaa6be2f5cb
DnyaNvB <dnya.nvb@gmail.com>
2021-07-24 06:18:10 +0430

    YOW3

--- src/main/java/controller/Scoreboard.java
+++ src/main/java/controller/Scoreboard.java
@@ -3,10 +3,24 @@ package controller;
 import server.model.User;
 import view.Show;
 
+import java.io.IOException;
+
 public class Scoreboard {
-    public static void showScoreboard() {
+    public static String showScoreboard() {
+        String result;
+        try {
+            Login.dataOut.writeUTF("scoreboard show");
+            Login.dataOut.flush();
+            result = Login.dataIn.readUTF();
+
+        } catch (IOException x) {
+            x.printStackTrace();
+            result = "show scoreboard error";
+        }
+        return result;
+
     //    User.sort(User.getAllUsers());
-        Show.showScoreBoard(User.getAllUsers());
+      //  Show.showScoreBoard(User.getAllUsers());
     }
     public static String menuName() {
         return "Scoreboard Menu";
