1e505e90516d2650e63c33a460d6df3ca2e5056c
DnyaNvB <dnya.nvb@gmail.com>
2021-07-25 22:42:26 +0430

    nemi

--- src/main/java/client/controller/Shop.java
+++ src/main/java/client/controller/Shop.java
@@ -5,7 +5,7 @@ import client.controller.Login;
 import java.io.IOException;
 
 public class Shop {
-    public static String buy(String cardName) {
+    public static synchronized String buy(String cardName) {
         String result;
         try {
             Login.dataOut.writeUTF("shop buy " + cardName);
@@ -17,12 +17,6 @@ public class Shop {
             result = "card shopping error";
         }
         return result;
-     //  Card card = Card.make(cardName);
-     //  if (card == null) return "there is no card with this name";
-     //  if (MainMenu.getCurrentUser().getMoney() < card.getPrice()) return "not enough money";
-     //  MainMenu.getCurrentUser().decreaseMoney(card.getPrice());
-     //  MainMenu.getCurrentUser().addCard(card);
-     //  return "shopping is successfully!";
     }
 
 
